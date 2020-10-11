b7733368d4bd75b75a9a263be5511c6401ab6643
jhankarpHero <61257321+jhankarpHero@users.noreply.github.com>
2020-10-11 08:40:24 -0500

    admin page content

--- src/components/AllPatients/AllPatients/AllPatients.js
+++ src/components/AllPatients/AllPatients/AllPatients.js
@@ -12,7 +12,7 @@ const AllPatients = () => {
     }, [])
 
     return (
-        <div className="container-fluid row " >
+        <div className="container-fluid row" >
             <Sidebar></Sidebar>
             <div className="col-md-10 p-4 pr-5" style={{ position: "absolute", right: 0, backgroundColor: "#F4FDFB" }}>
                 <h5 className="text-brand">All Patients</h5>
